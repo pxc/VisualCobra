ff7f147a4fa649bdab4d019a8aa1756c637f8d72
Matthew Strawbridge <matthew@philoxenic.com>
2011-02-07 10:43:51 +0000

    Added initial version of Visual Cobra Menu

---
+++ Visual Cobra Express/Visual Cobra Menu/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace VisualCobra.Visual_Cobra_Menu
+{
+    static class GuidList
+    {
+        public const string guidVisual_Cobra_MenuPkgString = "da390a51-3a52-4137-8450-582915e57e8c";
+        public const string guidVisual_Cobra_MenuCmdSetString = "c873ef57-927c-4728-a23a-8385d02dbeca";
+
+        public static readonly Guid guidVisual_Cobra_MenuCmdSet = new Guid(guidVisual_Cobra_MenuCmdSetString);
+    };
+}
---
+++ Visual Cobra Express/Visual Cobra Menu/PkgCmdID.cs
@@ -0,0 +1,12 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace VisualCobra.Visual_Cobra_Menu
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidCobraSettings =        0x100;
+        public const uint cmdidCobraRun =             0x101;
+    };
+}

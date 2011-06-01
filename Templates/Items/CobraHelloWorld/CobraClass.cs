f2894a874bc62c740ee57732fca7c924e83806a6
Matthew Strawbridge <matthew@philoxenic.com>
2011-06-01 22:01:35 +0100

    B&B before refactoring (non-Express files).

---
+++ Templates/Items/CobraHelloWorld/CobraClass.cs
@@ -0,0 +1,15 @@
+﻿/*
+
+"""
+This is the infamous "Hello, world." example.
+
+If everything is working, this file should appear in various colors.
+Select Cobra | Run to run it.
+"""
+
+class Hello
+
+    def main
+        print 'Hello, world.'
+
+*/
